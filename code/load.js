2fd546d7cdb5fbd7aa5678fda57de987eabce35b
sujinleeme <sujinlee.me@gmail.com>
2017-08-04 01:38:48 +0900

    Setup jekyll

--- code/load.js
+++
@@ -1,8 +0,0 @@
-// the kludge is used to
-// load dependency files into the global namespace, so that the
-// examples can run on node.
-
-module.exports = function() {
-  for (var i = 0; i < arguments.length; i++)
-    (1,eval)(require("fs").readFileSync(__dirname + "/../" + arguments[i], "utf8"));
-};
